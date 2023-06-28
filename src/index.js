d63bc24a7da38e3e92eff0b894ea8a93f30f8fb5
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-06-28 11:24:05 +0300

    first part ended

--- src/index.js
+++ src/index.js
@@ -1,27 +1,64 @@
 import { fetchCatByBreed } from "./cat-api";
+import { fetchBreeds } from "./cat-api";
+import { Notify } from "notiflix";
+import SlimSelect from 'slim-select'
 
-import (fetchCatByBreed)
 
 const selected = document.querySelector('.breed-select');
-const list = document.querySelector('.cat-info')
-selected.addEventListener('change', onChange)
+selected.setAttribute('id', 'slim');
+console.log(selected)
+const list = document.querySelector('.cat-info');
+list.innerHTML += '<div class = cat-info-desc></div>';
+const divCatDesc = document.querySelector('.cat-info-desc');
+const loader  = document.querySelector('.loader');
+const errorEl = document.querySelector('.error')
 
+
+
+selected.addEventListener('change', onChange)
+errorEl.hidden = true;
+
+renderBreedsList()
 
 function onChange(evt){
     evt.preventDefault();
-    const breedId = evt.currentTargeet.value;
-    console.log(breedId)
+    loader.hidden = true;
+    divCatDesc.innerHTML  = "";
+    const breedId = evt.currentTarget.value;
+    console.log(breedId);
+    fetchCatByBreed(breedId)
+    .then(breed => createMarkupCatDesc (breed))
+    .catch(err  => {console.log(err); Notify.failure('Oops! Something went wrong! Try reloading the page!')})
+    .finally(() => loader.hidden = true);
 }
-    // .then((data) => (list.innerHTML = createMarkup(data)))
-   
 
+    function creatMarkupOption(breeds){
+    const markup = breeds.map(breed =>{ return `<option value = "${breed.reference_image_id}">${breed.name}</option>`}).join('');
+        selected.insertAdjacentHTML('beforeend', markup);
+    }
 
-function createMarkup (arr){
-    return arr.map(({id, name, description, temperament}) => `<ul>
-    <li>
-    <img src="
-    " alt="${id}">
-  <h2>${name}</h2>
-<p>${description}</p>
-<p>${temperament}</p></li></ul>`).join('')
+function createMarkupCatDesc (breed){
+   const markup = `
+    <img src="${breed.url} " alt="${breed.id}" width ="300">
+  <h2>${breed.breeds[0].name}</h2>
+<p>${breed.breeds[0].description}</p>
+<p>Temperament:${breed.breeds[0].temperament}</p>`;
+divCatDesc.insertAdjacentHTML('beforeend', markup );
+// new SlimSelect({
+//     select: '#slim',
+//     settings: {
+//         openPosition: 'up' // 'auto', 'up' or 'down'
+//       }
+//   })
 }
+
+function renderBreedsList(){
+    loader.hidden = true;
+    fetchBreeds()
+    .then(breeds => creatMarkupOption(breeds))
+    .catch(error => console.log(error))
+    .finally(() => {loader.hidden = true;
+    selected.hidden = false}
+    )
+}
+  
